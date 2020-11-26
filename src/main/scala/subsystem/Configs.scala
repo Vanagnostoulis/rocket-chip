a2d93a2908c0b08bef1860cc91083aa04b5a0641
Vasileios Anagnostoulis (knikas diplom [v.anagn@yahoo.com]) <vanagn@termi1.cslab.ece.ntua.gr>
2020-11-26 19:42:22 +0200

    example accelerator added

--- src/main/scala/subsystem/Configs.scala
+++ src/main/scala/subsystem/Configs.scala
@@ -271,26 +271,40 @@ class WithNBreakpoints(hwbp: Int) extends Config ((site, here, up) => {
   }
 })
 
-class WithRoccExample extends Config((site, here, up) => {
-  case BuildRoCC => List(
-    (p: Parameters) => {
-        val accumulator = LazyModule(new AccumulatorExample(OpcodeSet.custom0, n = 4)(p))
-        accumulator
-    },
-    (p: Parameters) => {
-        val translator = LazyModule(new TranslatorExample(OpcodeSet.custom1)(p))
-        translator
-    },
-    (p: Parameters) => {
-        val counter = LazyModule(new CharacterCountExample(OpcodeSet.custom2)(p))
-        counter
-    },
-    (p: Parameters) => {
-      val blackbox = LazyModule(new BlackBoxExample(OpcodeSet.custom3, "RoccBlackBox")(p))
-      blackbox
-    })
-})
+class WithMyAccel extends Config ((site, here, up) => {
+  case BuildRoCC => 
+    Seq(
+      (p: Parameters) => 
+      {
+        val example = LazyModule(new ExampleAccel(OpcodeSet.custom0)(p))
+        example
+      }
+    )
+  }
+)
 
+class WithRoccAccumulator extends Config((site, here, up) => {
+  case BuildRoCC => 
+    Seq(
+      (p: Parameters) => 
+      {
+          val accumulator = LazyModule(new AccumulatorExample(OpcodeSet.custom2, n = 4)(p))
+          accumulator
+      }
+    )
+  }
+)
+
+class WithRoccTranslator extends Config((site, here, up) => {
+  case BuildRoCC => 
+    Seq(
+      (p: Parameters) => {
+          val translator = LazyModule(new TranslatorExample(OpcodeSet.custom1)(p))
+          translator
+      }
+    )
+  }
+)
 class WithDefaultBtb extends Config((site, here, up) => {
   case RocketTilesKey => up(RocketTilesKey, site) map { r =>
     r.copy(btb = Some(BTBParams()))
